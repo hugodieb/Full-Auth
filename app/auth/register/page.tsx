b054aa369dbd0228c8e2171689f957bf0f72f3b1
Hugo Dieb <hugodieb.hd@gmail.com>
2024-12-04 11:51:37 -0300

    tornando o form de autenticação dinamico

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,47 +1,8 @@
-'use client';
-
-import { ChangeEvent, FormEvent, useState } from "react";
 import Image from "next/image";
+import { RegisterForm } from "@/app/components/forms";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { toast } from "react-toastify";
-import Spinner from "@/app/components/common/Spinner";
-
-import { useRegisterMutation } from "@/redux/features/authApiSlice";
 
 export default function Page() {
-  const router = useRouter();
-  const [register, { isLoading }] = useRegisterMutation();
-
-  const [formData, setFormData] = useState({
-    first_name: "",
-    last_name: "",
-    email: "",
-    password: "",
-    re_password: "",
-  });
-
-  const { first_name, last_name, email, password, re_password } = formData;
-
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    register({ first_name, last_name, email, password, re_password })
-      .unwrap()
-      .then(() => {
-        toast.success('Please check email to verify account');
-        router.push('/auth/login');
-      })
-      .catch(() => {
-        toast.error('Failed to register account');
-      })
-  };
-
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -58,107 +19,7 @@ export default function Page() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-2" onSubmit={onSubmit}>
-          <div>
-            <label htmlFor="first_name" className="block text-sm/6 font-medium text-gray-900">
-              First Name
-            </label>
-            <div className="mt-2">
-              <input
-                id="first_name"
-                name="first_name"
-                type="text"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                value={first_name}
-                onChange={onChange}
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <label htmlFor="last_name" className="block text-sm/6 font-medium text-gray-900">
-              Last Name
-            </label>
-            <div className="mt-2">
-              <input
-                id="last_name"
-                name="last_name"
-                type="text"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                value={last_name}
-                onChange={onChange}
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                value={email}
-                onChange={onChange}
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center">
-              <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                value={password}
-                onChange={onChange}
-                required
-                autoComplete="current-password"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center">
-              <label htmlFor="re_password" className="block text-sm/6 font-medium text-gray-900">
-                Confirm Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="re_password"
-                name="re_password"
-                type="password"
-                value={re_password}
-                onChange={onChange}
-                required
-                autoComplete="current-password"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              {isLoading ? <Spinner sm /> : 'Sign Up'}
-            </button>
-          </div>
-        </form>
+        <RegisterForm />
 
         <p className="mt-2 text-center text-sm/6 text-gray-500">
           Already have an account?{' '}
---
+++ app/components/forms/Form.tsx
@@ -0,0 +1,47 @@
+import { FormEvent, ChangeEvent } from "react";
+import { Input } from '@/app/components/forms';
+import { Spinner } from "@/app/components/common";
+
+interface Config {
+  labelText: string;
+  labelId: string;
+  type: string;
+  value: string;
+  required?: boolean
+}
+
+interface Props {
+  config: Config[];
+  isLoading: boolean;
+  btnText: string
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+export default function Form({ config, isLoading, btnText, onChange, onSubmit }: Props) {
+  return (
+    <form className="space-y-2" onSubmit={onSubmit}>
+      {config.map(input => (
+        <Input
+          key={input.labelId}
+          labelId={input.labelId}
+          type={input.type}
+          onChange={onChange}
+          value={input.value}
+          required={input.required}
+        >
+          {input.labelText}
+        </Input>
+      ))}
+
+      <div className="mt-3">
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          {isLoading ? <Spinner sm /> : `${btnText}`}
+        </button>
+      </div>
+    </form>
+  );
+}
